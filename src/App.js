7d2ff3de110b42c9af7a5c6fb1fb4e88bf472a98
squashling <lornamutton@gmail.com>
2021-03-25 09:28:43 +0100

    added button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { AppTitle } from "assets/title";
+import { Button } from "components/Button";
 
 function App() {
   return (
     <div className="App">
       <AppTitle />
+      <Button label="Hello" id="1" onClick={() => console.log("click")} />
     </div>
   );
 }
