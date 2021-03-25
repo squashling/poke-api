7d0b02a92b2f6afd44b3f49a26063f44aa5379fa
charhagelin <charhagelin@gmail.com>
2021-03-25 20:25:17 +0100

    adding some styling for pokedex. grey box for image and game arrod for changin  åage

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -4,12 +4,22 @@ import { getPokemon } from "redux/ducks/pokedex";
 
 export default function Pokedex() {
   const pokemonList = useSelector((state) => state.pokedex.pokemonList);
+  const offset = useSelector((state) => state.pokedex.offset);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPokemon(1));
+    dispatch(getPokemon(offset));
   }, []);
 
   console.log(pokemonList);
-  return <div>pokedex</div>;
+  return (
+    <div className="pokedex-wrapper">
+      pokedex
+      <div className="pokedex-window"></div>
+      <div className="game-arrows-wrapper">
+        <div className="game-arrows-vertical"></div>
+        <div className="game-arrows-horizontal"></div>
+      </div>
+    </div>
+  );
 }
--- src/redux/ducks/pokedex.js
+++ src/redux/ducks/pokedex.js
@@ -1,31 +1,38 @@
 import axios from "axios";
 import { SET_POKEMON } from "redux/types";
 
-export const getPokemon = (page) => {
+export const getPokemon = (offset) => {
   return (dispatch) => {
     return axios
-      .get(process.env.REACT_APP_API_HOST + "pokemon?limit=100&offset=200")
+      .get(process.env.REACT_APP_API_HOST + "pokemon?limit=20&offset=" + offset)
       .then((res) => {
         console.log(res);
-        dispatch(setPokemon());
+        dispatch(setPokemon(res.data.results));
       })
       .catch((err) => console.log(err));
   };
 };
 
-const setPokemon = (pokemon) => {
+const setPokemon = (pokemon, offset) => {
   return {
     type: SET_POKEMON,
     pokemonList: pokemon,
+    offset,
   };
 };
 
 const initialState = {
   pokemonList: [],
+  offset: 40,
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
+    case SET_POKEMON:
+      return {
+        ...state,
+        pokemonList: action.pokemonList,
+      };
     default:
       return state;
   }
