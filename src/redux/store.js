cd49ebf83ccc5aaf734082a7facfd3c242252150
Charlie Hagelin <charliehagelin@Charlies-MacBook-Pro.local>
2021-03-25 06:22:13 +0100

    changing strucutre adding code to store

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers";
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
+);
+
+export default store;
