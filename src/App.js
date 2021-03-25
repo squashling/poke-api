cc220d637565c9d7404ed7505170d2550c52163d
Charlie Hagelin <charliehagelin@Charlies-MacBook-Pro.local>
2021-03-25 09:16:08 +0100

    swtich statement for chaning view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import { AppTitle } from "assets/title";
+import Home from "Home";
 
 function App() {
+  const [view, setView] = useState("home");
+
+  useEffect(() => {
+    renderView();
+  }, [view]);
+
+  const renderView = () => {
+    switch (view) {
+      case "home":
+        return <Home />;
+      default:
+        return <Home />;
+    }
+  };
+
+  const changeView = (view) => {
+    setView(view);
+  };
+
   return (
     <div className="App">
       <AppTitle />
+      {renderView()}
     </div>
   );
 }
