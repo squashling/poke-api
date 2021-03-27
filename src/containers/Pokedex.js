8cf1d8eee3544137329dd9ec95bba79fb4da8ead
charhagelin <charhagelin@gmail.com>
2021-03-27 10:18:31 +0100

    adding animation to images in pokedex

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -7,13 +7,20 @@ export default function Pokedex() {
   const offset = useSelector((state) => state.pokedex.offset);
   const dispatch = useDispatch();
   const [pokeIndex, setPokemonIndex] = useState(0);
+  const [xAxis, setXAxis] = useState(0);
 
   useEffect(() => {
     dispatch(getPokemon(offset));
   }, [dispatch, offset]);
 
+  useEffect(() => {
+    dispatch(getPokemon(offset));
+  }, [pokeIndex]);
+
   const nextPokemon = () => {
     if (pokeIndex < pokemonList.length - 1) {
+      imageSlideRight();
+
       let newIndex = pokeIndex + 1;
       setPokemonIndex(newIndex);
     } else if (pokeIndex === pokemonList.length - 1) {
@@ -26,6 +33,8 @@ export default function Pokedex() {
 
   const prevPokemon = () => {
     if (pokeIndex > 0) {
+      imageSlideLeft();
+
       let newIndex = pokeIndex - 1;
       setPokemonIndex(newIndex);
     } else if (pokeIndex === 0 && offset > 20) {
@@ -34,13 +43,60 @@ export default function Pokedex() {
       dispatch(getPokemon(newOffset));
     } else return;
   };
+  const imageSlideRight = () => {
+    let imageLength = pokemonList.length - 1;
+    // width of one image (in pixels)
+    let widthImg = 40;
+    let newXAxis = xAxis;
+    let slider = document.getElementById("image-slider-container");
+    newXAxis += widthImg;
+    newXAxis %= imageLength * widthImg;
+    console.log("delta", newXAxis);
+    setXAxis(newXAxis);
+
+    slider.querySelector("#pokedex-window").style.marginLeft =
+      "-" + newXAxis + "rem";
+  };
+  const imageSlideLeft = () => {
+    let imageLength = pokemonList.length - 1;
+    // width of one image (in pixels)
+    let widthImg = 40;
+    let newXAxis = xAxis;
+    let slider = document.getElementById("image-slider-container");
+    newXAxis -= widthImg;
+    newXAxis %= imageLength * widthImg;
+    console.log("delta", newXAxis);
+    setXAxis(newXAxis);
+    slider.querySelector("#pokedex-window").style.marginLeft =
+      "-" + newXAxis + "rem";
+  };
   return (
     <div className="pokedex-wrapper">
-      <div className="pokedex-window">
-        <img
-          className="pokemon-sprite"
-          src={pokemonList[pokeIndex].sprites.front_default}
-        />
+      <div id="image-slider-container" className="image-slider-container">
+        <div id="pokedex-window" className="pokedex-window">
+          {pokemonList.map((pokemon) => (
+            <img
+              className="pokemon-sprite "
+              src={pokemon.sprites.front_default}
+            />
+          ))}
+          {/* {pokeIndex !== 0 && (
+            <img
+              className="pokemon-sprite  "
+              src={pokemonList[pokeIndex + 1]?.sprites.front_default}
+            />
+          )}
+          <img
+            className="pokemon-sprite "
+            src={pokemonList[pokeIndex]?.sprites.front_default}
+          />
+          {pokeIndex !== pokemonList.length - 1 && (
+            <img
+              className="pokemon-sprite "
+              src={pokemonList[pokeIndex - 1]?.sprites.front_default}
+            />
+          )} */}
+        </div>
       </div>
       <div className="game-arrows-wrapper">
         <div className="game-arrows-vertical"></div>
